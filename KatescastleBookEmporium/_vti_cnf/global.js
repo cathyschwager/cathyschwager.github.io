eb5a58e2c190da0e6968dedaa96b718b9368935e
Gregary Boyles <gregplants@bigpond.com>
2023-07-26 03:14:22 +1000

    Add files via upload

---
+++ KatescastleBookEmporium/_vti_cnf/global.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|DESKTOP-KFJ8F7O\\boyle
+vti_modifiedby:SR|DESKTOP-KFJ8F7O\\boyle
+vti_timelastmodified:TR|25 Jul 2023 14:31:53 -0000
+vti_timecreated:TR|24 Jul 2023 05:03:10 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|NonFiction/Sports/Sports.html NonFiction/Hobbies/Hobbies.html NonFiction/Humour/Humour.html NonFiction/Reference/Reference.html NonFiction/Technology/Technology.html Emporium/Emporium.html Specialist/NationalGeographic/NationalGeographic.html NonFiction/Travel/Travel.html Facebook/Facebook.html Fiction/Action/Action.html Fiction/General/General.html Fiction/Thrillers/Thrillers.html NonFiction/Outdoors/Outdoors.html Specialist/AntiquePre1950/AntiquePre1950.html NonFiction/World/World.html FAQ/FAQ.html Specialist/ReadersDigestCondensed/ReadersDigestCondensed.html Requests/Requests.html index.htm NonFiction/AutoBiographies/AutoBiographies.html NonFiction/Crafts/Crafts.html NonFiction/Gardening/Gardening.html NonFiction/Religion/Religion.html Specialist/BoxSets/BoxSets.html Specialist/Series/Series.html master.dwt Fiction/Mystery/Mystery.html NonFiction/Cooking/Cooking.html Specialist/FirstEditions/FirstEditions.html Specialist/Penguin/Penguin.html Specialist/Vintage1950To1975/Vintage1950To1975.html Specialist/Miscellaneous/Miscellaneous.html Fiction/Crime/Crime.html Fiction/Romance/Romance.html Fiction/Sci-Fi\\ Fantasy/Sci-Fi\\ Fantasy.html Fiction/Westerns/Westerns.html YoungAdults/YoungAdults.html Fiction/Sagas/Sagas.html NonFiction/Health/Health.html Specialist/Shakespeare/Shakespeare.html Fiction/Horror/Horror.html NonFiction/Education/Education.html Specialist/MillsAndBoon/MillsAndBoon.html Specialist/ProlificAuthors/ProlificAuthors.html Specialist/Retro1975To2000/Retro1975To2000.html Children/Children.html NonFiction/Arts/Arts.html Specialist/Classics/Classics.html
+vti_nexttolasttimemodified:TW|25 Jul 2023 14:31:26 -0000
+vti_cacheddtm:TX|25 Jul 2023 14:31:53 -0000
+vti_filesize:IR|13990
