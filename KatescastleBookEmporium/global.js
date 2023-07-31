833b8a7460a332ad06e449db65cb579ed86b7e4a
Gregary Boyles <gregplants@bigpond.com>
2023-07-31 20:57:38 +1000

    Add files via upload

--- KatescastleBookEmporium/global.js
+++ KatescastleBookEmporium/global.js
@@ -1,4 +1,4 @@
-﻿﻿//********************************************************************************************************************************
+﻿//********************************************************************************************************************************
 //********************************************************************************************************************************
 // GLOBAL VARIBALES
 //********************************************************************************************************************************
@@ -8,8 +8,11 @@ var g_arrayCategoryBookLists = [];
 var g_bFictionPopupMenu = false, g_bNonFictionPopupMenu = false, g_bSpecialistPopupMenu = false;
 var g_strURL = "https://cathyschwager.github.io/KatescastleBookEmporium";
 var g_strEmail = "katescastle" + "@" + "ozemail" + "." + "com" + "." + "au";
-var g_strItemsShoppingCartItems4Email = "";
-var g_fShoppingCartTotal4Email = 0;
+var g_structOrderDetails = {
+								strShoppingCartItems: "",
+								fShoppingCartTotal: 0,
+								fShoppingCartTotalMass: 0
+							};
 
 function GenerateGregsEmailAddress()
 {
@@ -19,7 +22,7 @@ function GenerateGregsEmailAddress()
 
 function GenerateCathysEmailAddress()
 {
-	document.write("<a class=\"Email\" href=\"mailto: " + g_strEmail + "\">" + strEmailAddress + "</a>");
+	document.write("<a class=\"Email\" href=\"mailto: " + g_strEmail + "\">" + g_strEmail + "</a>");
 }
 
 function GenerateMenu(arrayCategoryBookmarks)
@@ -123,7 +126,7 @@ function GeneratePageContents(arrayCategoryBookmarks, arrayCategoryBookLists)
 						divItems.innerHTML += "<button type=\"button\" style=\"display: none;\" id=\"AddCart" + arrayBookList[nJ][0] + arrayBookList[nJ][1] + 
 										"\" class=\"cart_button\" onclick=\"OnClickAddCartButton('" + arrayBookList[nJ][0] + 
 										"', '" + arrayBookList[nJ][1] + "', '" + arrayBookList[nJ][2] + "', '" + arrayBookList[nJ][3] + 
-										"', '" + arrayBookList[nJ][4] + 
+										"', '" + arrayBookList[nJ][4] + "', '" + arrayBookList[nJ][5] + 
 										"')\"><img src=\"../images/add_shopping_cart.jpg\" alt=\"Add to cart\" /></button>&nbsp;";
 						divItems.innerHTML += "<button type=\"button\" id=\"RemoveCart" + arrayBookList[nJ][0] + 
 										arrayBookList[nJ][1] + "\" class=\"cart_button\" onclick=\"OnRemoveCartButton('" + 
@@ -135,7 +138,7 @@ function GeneratePageContents(arrayCategoryBookmarks, arrayCategoryBookLists)
 						divItems.innerHTML += "<button type=\"button\" id=\"AddCart" + arrayBookList[nJ][0] + arrayBookList[nJ][1] + 
 										"\" class=\"cart_button\" onclick=\"OnClickAddCartButton('" + arrayBookList[nJ][0] + 
 										"', '" + arrayBookList[nJ][1] + "', '" + arrayBookList[nJ][2] + "', '" + arrayBookList[nJ][3] + 
-										"', '" + arrayBookList[nJ][4] + 
+										"', '" + arrayBookList[nJ][4] + "', '" + arrayBookList[nJ][5] +
 										"')\"><img src=\"../images/add_shopping_cart.jpg\" alt=\"Add to cart\" /></button>&nbsp;";
 						divItems.innerHTML += "<button type=\"button\" style=\"display: none;\" id=\"RemoveCart" + arrayBookList[nJ][0] + 
 										arrayBookList[nJ][1] + "\" class=\"cart_button\" onclick=\"OnRemoveCartButton('" + 
@@ -169,7 +172,7 @@ function ShowEmptyShoppingCartLink(bShow)
 	}
 }
 
-function OnClickAddCartButton(strTitle, strAuthor, strPrice, strDesc, strImage)
+function OnClickAddCartButton(strTitle, strAuthor, strPrice, strDesc, strImage, strMass)
 {
 	document.getElementById("AddCart" + strTitle + strAuthor).style.display = "none";
 	document.getElementById("RemoveCart" + strTitle + strAuthor).style.display = "inline-block";
@@ -193,14 +196,32 @@ function OnClickAddCartButton(strTitle, strAuthor, strPrice, strDesc, strImage)
 	if (nI === -1)
 	{
 		// ["Title 1", 4.00, "Fred Smith", "Book description 1", "image.jpg"]
-		arrayShoppingCart.push([strTitle, strAuthor, strPrice, strDesc, strImage]);
+		arrayShoppingCart.push([strTitle, strAuthor, strPrice, strDesc, strImage, strMass]);
 	}
 	else
 	{
-		arrayShoppingCart[nI] = [strTitle, strAuthor, strPrice, strDesc, strImage];
+		arrayShoppingCart[nI] = [strTitle, strAuthor, strPrice, strDesc, strImage, strMass];
 	}
 	sessionStorage["ShoppingCart"] = JSON.stringify(arrayShoppingCart);
 	ShowEmptyShoppingCartLink(true);
+}
+
+function GetlSelectedIndex(Select, strTextToSelect)
+{
+	var nSelectionIndex = 0;
+	
+	if (Select)
+	{
+		for (let nI = 0; nI < Select.options.length; nI++)
+		{
+			if (Select.options[nI].value == strTextToSelect)
+			{
+				nSelectionIndex = nI;
+				break;
+			}
+		}
+	}
+	return nSelectionIndex;
 }
 
 function GenerateShoppingContents(divShoppingCart)
@@ -219,7 +240,10 @@ function GenerateShoppingContents(divShoppingCart)
 			divShoppingCart.innerHTML += "<p >&nbsp;</p>";
 	}
 	else
-	{	
+	{
+		g_structOrderDetails.strShoppingCartItems = "";
+		g_structOrderDetails.fShoppingCartTotal = g_structOrderDetails.fShoppingCartTotalMass = 0;
+		
 		for (let nI = 0; nI < arrayShoppingCart.length; nI++)
 		{
 			divShoppingCart.innerHTML += "<p class=\"Paragraph\"><b>Title: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b>" + arrayShoppingCart[nI][0] + "<br/>" +
@@ -227,7 +251,7 @@ function GenerateShoppingContents(divShoppingCart)
 										"<b>Price: &nbsp;&nbsp;&nbsp;</b>$" + arrayShoppingCart[nI][2] + "<br/>" + 
 										"<b><u>Description</u></b><br/>" + arrayShoppingCart[nI][3] + "<br/>" +
 										"<img width=\"100\" alt=\"images/" + arrayShoppingCart[nI][4] + "\"" + 
-										"src=\"" + g_strURL + arrayShoppingCart[nI][4] + "\" /><br/>" +
+										"src=\"" + g_strURL + arrayShoppingCart[nI][4] + "\" /><br/><br/>" +
 										
 										"<button type=\"button\" class=\"cart_button\" onclick=\"OnRemoveCartButton('" + 
 										arrayShoppingCart[nI][0] + "', '" + arrayShoppingCart[nI][1] + 
@@ -241,15 +265,55 @@ function GenerateShoppingContents(divShoppingCart)
 										"<img src=\"../images/continue_shopping.jpg\" alt=\"Continue shopping\" /></button></p>" +
 										*/
 										"<br/><hr><br/><br/>";
-										g_strItemsShoppingCartItems4Email+= arrayShoppingCart[nI][0] + ", " + 
+										g_structOrderDetails.strShoppingCartItems += arrayShoppingCart[nI][0] + ", " + 
 												arrayShoppingCart[nI][1] + ", " + arrayShoppingCart[nI][2] + "\n";
-										g_fShoppingCartTotal4Email += Number(arrayShoppingCart[nI][2]);
+										g_structOrderDetails.fShoppingCartTotal += Number(arrayShoppingCart[nI][2]);
+										g_structOrderDetails.fShoppingCartTotalMass += Number(arrayShoppingCart[nI][5]);
 		}
 	}
 	divShoppingCart.innerHTML += document.getElementById("OrderForm").innerHTML;
 	strSubmitOrder = "<a class=\"SubmitOrderButton\" id=\"SubmitOrderButton\" href=\"\">SUBMIT ORDER</a><br/><br/>";
 	divShoppingCart.innerHTML += strSubmitOrder;
 	OnStateChange();
+
+	if (sessionStorage["TextGivenNames"])
+		document.getElementById("TextGivenNames").value = sessionStorage["TextGivenNames"];
+	if (sessionStorage["TextSurname"])
+		document.getElementById("TextSurname").value = sessionStorage["TextSurname"];
+	if (sessionStorage["TextEmail"])
+		document.getElementById("TextEmail").value = sessionStorage["TextEmail"];
+	if (sessionStorage["TextPhoneNumber"])
+		document.getElementById("TextPhoneNumber").value = sessionStorage["TextPhoneNumber"];
+	if (sessionStorage["TextAddress"])
+		document.getElementById("TextAddress").value = sessionStorage["TextAddress"];
+	if (sessionStorage["TextSuburb"])
+		document.getElementById("TextSuburb").value = sessionStorage["TextSuburb"];
+	if (sessionStorage["SelState"])
+		document.getElementById("SelState").selectedIndex = GetlSelectedIndex(document.getElementById("SelState"), sessionStorage["SelState"]);
+	if (sessionStorage["SelPostcode"])
+		document.getElementById("SelPostcode").selectedIndex = GetlSelectedIndex(document.getElementById("SelPostcode"), sessionStorage["SelPostcode"]);	
+}
+
+function DoOnChangeSuburb()
+{
+	var textSuburb = document.getElementById("TextSuburb");
+	
+	if (textSuburb)
+	{
+	}
+}
+
+function DoEraseDetails()
+{
+	document.getElementById("TextGivenNames").value = "";
+	document.getElementById("TextSurname").value = "";
+	document.getElementById("TextEmail").value = "";
+	document.getElementById("TextPhoneNumber").value = "";
+	document.getElementById("TextAddress").value = "";
+	document.getElementById("TextSuburb").value = "";
+	document.getElementById("SelState").selectedIndex = 0;
+	document.getElementById("SelPostcode").selectedIndex = 0;
+	OnStateChange();	
 }
 
 function isValidEmail(strEmail)
@@ -316,29 +380,183 @@ function isValidSurname(strName)
 	return bIsValid;
 }
 
+function DoGetAustraliaPostZone(nPostcode)
+{
+	var strZone = "";
+	
+	if (((nPostcode >= 200) && (nPostcode <= 299)) ||
+		((nPostcode >= 2487) && (nPostcode <= 2499)) ||
+		((nPostcode >= 2531) && (nPostcode <= 2554)) ||
+		((nPostcode >= 2787) && (nPostcode <= 2879)) ||
+		((nPostcode >= 2881) && (nPostcode <= 2889)) ||
+		((nPostcode >= 2891) && (nPostcode <= 2898)) ||
+		((nPostcode >= 2900) && (nPostcode <= 2999)))
+		strZone = "N2";
+	else if (((nPostcode >= 1000) && (nPostcode <= 2263)) ||
+			 ((nPostcode >= 2500) && (nPostcode <= 2530)) ||
+			 ((nPostcode >= 2555) && (nPostcode <= 2574)) ||
+			 (nPostcode == 2890))
+		strZone = "N1";
+	else if ((nPostcode == 2648) || (nPostcode == 2715) || 
+			 ((nPostcode >= 2717) && (nPostcode <= 2719)) ||
+			 ((nPostcode >= 2731) && (nPostcode <= 2739)) ||
+			 ((nPostcode >= 3221) && (nPostcode <= 3334)) ||
+			 ((nPostcode >= 3342) && (nPostcode <= 3424)) ||
+			 ((nPostcode >= 3444) && (nPostcode <= 3688)) ||
+			 ((nPostcode >= 3691) && (nPostcode <= 3749)) ||
+			 ((nPostcode >= 3812) && (nPostcode <= 3909)) ||
+			 ((nPostcode >= 3921) && (nPostcode <= 3925)) ||
+			 ((nPostcode >= 3945) && (nPostcode <= 3971)) ||
+			 (nPostcode == 3979) ||
+			 ((nPostcode >= 3984) && (nPostcode <= 3999)))
+		strZone = "V2";
+	else if (((nPostcode >= 3000) && (nPostcode <= 3220)) ||
+			 ((nPostcode >= 3926) && (nPostcode <= 3944)) ||
+			 ((nPostcode >= 3972) && (nPostcode <= 3978)) ||
+			 ((nPostcode >= 3980) && (nPostcode <= 3983)) ||
+			 ((nPostcode >= 8000) && (nPostcode <= 8999)))
+		strZone = "V1";
+	else if (((nPostcode >= 4000) && (nPostcode <= 4224)) ||
+			 ((nPostcode >= 4226) && (nPostcode <= 4299)) ||
+			 ((nPostcode >= 4500) && (nPostcode <= 4549)) ||
+			 ((nPostcode >= 9000) && (nPostcode <= 9299)) ||
+			 ((nPostcode >= 9400) && (nPostcode <= 9596)) ||
+			 ((nPostcode >= 9700) && (nPostcode <= 9799)))
+		strZone = "Q1";
+	else if (((nPostcode >= 4300) && (nPostcode <= 4449)) ||
+			 ((nPostcode >= 4550) && (nPostcode <= 4699)) ||
+			 ((nPostcode >= 9597) && (nPostcode <= 9599)) ||
+			 ((nPostcode >= 9880) && (nPostcode <= 9919)))
+		strZone = "Q2";
+	else if (((nPostcode >= 4450) && (nPostcode <= 4499)) ||
+			 ((nPostcode >= 4700) && (nPostcode <= 4805)) ||
+			 ((nPostcode >= 9920) && (nPostcode <= 9959)))
+		strZone = "Q3";
+	else if ((nPostcode >= 4806) && (nPostcode <= 4899))
+		strZone = "Q4";
+	else if (((nPostcode >= 5000) && (nPostcode <= 5199)) ||
+			 ((nPostcode >= 5800) && (nPostcode <= 5999)))
+		strZone = "S1";
+	else if ((nPostcode >= 5200) && (nPostcode <= 5749))
+		strZone = "S2";
+	else if (((nPostcode >= 800) && (nPostcode <= 999)))
+		strZone = "NT1";
+	else if (((nPostcode >= 6000) && (nPostcode <= 6214)) ||
+			 ((nPostcode >= 6800) && (nPostcode <= 6999)))
+		strZone = "W1";
+	else if ((nPostcode >= 6215) && (nPostcode <= 6699))
+		strZone = "W2";
+	else if ((nPostcode >= 6700) && (nPostcode <= 6797))
+		strZone = "W3";
+	else if ((nPostcode >= 6798) && (nPostcode <= 6799))
+		strZone = "W4";
+	else if (((nPostcode >= 7000) && (nPostcode <= 7150)) ||
+			 ((nPostcode >= 7155) && (nPostcode <= 7999)))
+		strZone = "T1";
+	else if ((nPostcode >= 7151) && (nPostcode <= 7154))
+		strZone = "AAT";
+	else if ((nPostcode >= 2485) && (nPostcode <= 2486))
+		strZone = "Q1";
+	else if (((nPostcode >= 2640) && (nPostcode <= 2641)) || 
+			 ((nPostcode >= 3689) && (nPostcode <= 3690)))
+		strZone = "V2";
+	else if (nPostcode == 4225)
+		strZone = "Q2";
+		
+	return strZone;
+}
+
+function DoGetDistanceCharge(strZone)
+{
+	var fUnitCostPerKg = 0;
+	
+	if (strZone == "N1")
+		fUnitCostPerKg = 2.25;
+	else if (strZone == "N2")
+		fUnitCostPerKg = 2.45;
+	else if (strZone == "V1")
+		fUnitCostPerKg = 1.85;
+	else if (strZone == "V2")
+		fUnitCostPerKg = 1.85;
+	else if (strZone == "Q1")
+		fUnitCostPerKg = 4.10;
+	else if (strZone == "Q2")
+		fUnitCostPerKg = 5.10;
+	else if (strZone == "Q3")
+		fUnitCostPerKg = 5.25;
+	else if (strZone == "Q4")
+		fUnitCostPerKg = 6.85;
+	else if (strZone == "S1")
+		fUnitCostPerKg = 2.25;
+	else if (strZone == "S2")
+		fUnitCostPerKg = 3.40;
+	else if (strZone == "NT1")
+		fUnitCostPerKg = 8.15;
+	else if (strZone == "W1")
+		fUnitCostPerKg = 6.70;
+	else if (strZone == "W2")
+		fUnitCostPerKg = 8.00;
+	else if (strZone == "W3")
+		fUnitCostPerKg = 8.85;
+	else if (strZone == "T1")
+		fUnitCostPerKg = 3.10;
+	else if (strZone == "NF")
+		fUnitCostPerKg = 6.70;
+	else if (strZone == "W4")
+		fUnitCostPerKg = 8.00;
+	else if (strZone == "AAT")
+		fUnitCostPerKg = 2.95;	
+	
+	return fUnitCostPerKg;
+}
+
+function DoCalculatePostage(strPostcode, fTotalMass)
+{
+	var strZone = DoGetAustraliaPostZone(Number(strPostcode)),
+		fUnitCostPerKg = DoGetDistanceCharge(strZone),
+		fPostage = 21.95 + ((Number(fTotalMass) / 1000) * fUnitCostPerKg);
+	
+	return fPostage;
+}
+
 function DoValidateOrderDetails()
 {
 	var textGivenNames = document.getElementById("TextGivenNames"), textSurname = document.getElementById("TextSurname"),
 		textEmail = document.getElementById("TextEmail"), textPhoneNumber = document.getElementById("TextPhoneNumber"),
 		textAddress = document.getElementById("TextAddress"), selState = document.getElementById("SelState"),
 		textSuburb = document.getElementById("TextSuburb"), selPostcode = document.getElementById("SelPostcode"),
-		linkSubmitOrder = document.getElementById("SubmitOrderButton");
+		linkSubmitOrder = document.getElementById("SubmitOrderButton"), fPostage = 0;
 
 	if (linkSubmitOrder && textGivenNames && textSurname && textEmail && textPhoneNumber && selState && textSuburb && selPostcode && textAddress)
 	{
 		if (isValidGivenNames(textGivenNames.value) && isValidSurname(textSurname.value) && isValidEmail(textEmail.value) && isValidPhoneNumber(textPhoneNumber.value))
 		{
+			fPostage = DoCalculatePostage(selPostcode.options[selPostcode.selectedIndex].value, g_structOrderDetails.fShoppingCartTotalMass) + 0;
+			
 			linkSubmitOrder.href = "mailto: " + g_strEmail + "?" +
 							"subject=BOOK ORDER&body=Name: " + textGivenNames.value + " " + textSurname.value + "%0D%0A" +
 							"Email: " + textEmail.value + "%0D%0A" +  "Mobile: " + textPhoneNumber.value + "%0D%0A%0D%0A" +
 							
-							"Unit / Street" + textAddress.value + "%0D%0A" +
-							"Suburb/ Town: " + textSuburb.value + "%0D%0A" +
+							"Unit/Street: " + textAddress.value + "%0D%0A" +
+							"City/Suburb/Town: " + textSuburb.value + "%0D%0A" +
 							"State: " + selState.options[selState.selectedIndex].value + "%0D%0A" +
 							"Postcode: " + selPostcode.options[selPostcode.selectedIndex].value + "%0D%0A%0D%0A" +
 
-							g_strItemsShoppingCartItems4Email + "%0D%0A%0D%0AORDER TOTAL: $" + g_fShoppingCartTotal4Email.toFixed(2) + 
-							"%0D%0A";				
+							g_structOrderDetails.strShoppingCartItems + "%0D%0A%0D%0AORDER SUBTOTAL: $" + g_structOrderDetails.fShoppingCartTotal.toFixed(2) + 
+							"%0D%0APOSTAGE & HANDLING: $" + fPostage.toFixed(2) + "%0D%0ATOTAL: $" + (fPostage + g_structOrderDetails.fShoppingCartTotal).toFixed(2);
+			
+			sessionStorage["TextGivenNames"] = document.getElementById("TextGivenNames").value;
+			sessionStorage["TextSurname"] = document.getElementById("TextSurname").value;
+			sessionStorage["TextEmail"] = document.getElementById("TextEmail").value;
+			sessionStorage["TextPhoneNumber"] = document.getElementById("TextPhoneNumber").value;
+			sessionStorage["TextAddress"] = document.getElementById("TextAddress").value;
+			sessionStorage["TextSuburb"] = document.getElementById("TextSuburb").value;
+			sessionStorage["SelState"] = document.getElementById("SelState").options[document.getElementById("SelState").selectedIndex].value;
+			sessionStorage["SelPostcode"] = document.getElementById("SelPostcode").options[document.getElementById("SelPostcode").selectedIndex].value;
+		
+			document.getElementById("TextSubtotal").value = "$" + g_structOrderDetails.fShoppingCartTotal.toFixed(2);
+			document.getElementById("TextPostage").value = "$" + fPostage.toFixed(2);
+			document.getElementById("TextTotal").value = "$" + (g_structOrderDetails.fShoppingCartTotal + fPostage).toFixed(2);
 			linkSubmitOrder.style.display="inline";
 		}
 	}
